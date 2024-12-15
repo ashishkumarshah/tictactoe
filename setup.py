f018339387f23b13d333bac2bd80c3fc36e875ed
Ashish Kumar Shah <ashish@Ashishs-Mac-mini.local>
2024-12-15 13:58:52 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tic-tac-toe',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Ashish Kumar Shah',
+    author_email='',
+    description=''
+)
